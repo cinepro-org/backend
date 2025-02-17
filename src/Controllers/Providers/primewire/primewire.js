d61b24001dc887edbe7db15ad7b163e1a505bfc8
An0n-00 <142223573+An0n-00@users.noreply.github.com>
2025-02-17 16:24:33 +0100

    refactor: enhance PrimeWire provider to support MixDrop and Streamtape links (direct mp4)

--- src/Controllers/Providers/primewire/primewire.js
+++ src/Controllers/Providers/primewire/primewire.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import * as cheerio from "cheerio";
 import * as crypto from "crypto";
 import fetch from "node-fetch";
-import * as worker_threads from "node:worker_threads";
+import * as url from "node:url";
+import {THROW} from "hls-parser/utils.js";
 
 const URL = "https://www.primewire.tf";
 const DS_KEY = "JyjId97F9PVqUPuMO0";
@@ -14,14 +15,24 @@ export async function getPrimewire(info) {
 
     const link = await lookupPage(info);
     const servers = await loadServers(link);
-    const embeddableServers = await Promise.all(servers.map(server => getEmbedLink(server)));
-
-    const files = embeddableServers.map(embedLink => ({
-        file: embedLink,
-        type: "embed",
-        quality: "unknown",
-        lang: "en"
+    const embeddableServers = await Promise.all(servers.map(async server => {
+        if (server.includes("mixdrop") || server.includes("mxdrop")) {
+            return await getEmbedLinkForMixDrop(server);
+        } else if (server.includes("streamtape")) {
+            return await getStreamtapeUrl(server);
+        } else {
+            return {videoLink: server, quality: "unknown", type: "embed"};
+        }
     }));
+
+    const files = embeddableServers
+        .filter(embedLink => embedLink.videoLink)
+        .map(embedLink => ({
+            file: embedLink.videoLink,
+            type: embedLink.type,
+            quality: embedLink.quality || "unknown",
+            lang: "en"
+        }));
 
     return {
         provider: "primewire",
@@ -59,7 +70,7 @@ async function lookupPage(info) {
 }
 
 async function loadServers(link) {
-    if (!link) return [];
+    if (!link) return new Error("[primewire] No link found");
 
     let website = await fetch(link);
     website = await website.text();
@@ -81,12 +92,18 @@ function sha1Hex(str) {
     return crypto.createHash('sha1').update(str).digest('hex');
 }
 
-async function getEmbedLink(server) {
+async function getEmbedLinkForMixDrop(server) {
     let website = await fetch(server);
     website = await website.text();
     let $ = cheerio.load(website);
+    if (!$("textarea").length) {
+        return null;
+    }
     let embedLink = $("textarea").text().match(/src="(.+?)"/)[1];
-    return await getVideoFromEmbed(embedLink);
+    let title = $(".title").text();
+    let quality = title.match(/.(\d+p)./)[1];
+    let videoLink = await getVideoFromEmbed(embedLink, quality);
+    return {videoLink, quality};
 }
 
 async function getVideoFromEmbed(embedLink) {
@@ -97,10 +114,56 @@ async function getVideoFromEmbed(embedLink) {
     }
     embedLink = embedLink.replace("mixdrop.ps/e", "mixdrop.ps/f");
     let $ = cheerio.load(await (await fetch(embedLink)).text());
-    // await $("a.btn.btn3.download-btn").click();
+    
+    // make post request with csrf token, token (from google captcha :( ) and a=genticket
+    let csrfToken = $("meta[name=csrf]").attr("content");
+    
     let downloadLink = await $("a.btn.btn3.download-btn").attr("href");
     if (!downloadLink) {
         return embedLink;
     }
     return downloadLink;
+}
+
+async function getStreamtapeUrl(url) {
+    try {
+        
+        let hostname = url.match(/https?:\/\/([^\/]+)/)[1];
+        const response = await fetch(url);
+
+        const html = await response.text();
+
+        const urlRegex =
+            /document\.getElementById\('norobotlink'\)\.innerHTML = (.*);/;
+        const urlMatch = html.match(urlRegex);
+        if (!urlMatch) throw new Error("norobotlink url not found");
+
+        const tokenRegex = /token=([^&']+)/;
+        const tokenMatch = urlMatch[1].match(tokenRegex);
+        if (!tokenMatch) throw new Error("token not found");
+
+        const fullUrlRegex =
+            /<div id="ideoooolink" style="display:none;">(.*)<[/]div>/;
+        const fullUrlMatch = html.match(fullUrlRegex);
+        if (!fullUrlMatch) throw new Error("ideoooolink url not found");
+
+        let finalUrl = fullUrlMatch[1].split(hostname)[1];
+        finalUrl = `https://${hostname}${finalUrl}&token=${tokenMatch[1]}`;
+
+        const titleRegex = /<meta name="og:title" content="(.*)">/;
+        const titleMatch = html.match(titleRegex);
+        let quality = titleMatch[1].match(/.(\d+)p./)[1];
+
+        return {
+            videoLink: finalUrl + "&dl=1",
+            quality: quality + 'p',
+            type: "direct"
+        };
+    } catch (error) {
+        return {
+            videoLink: url,
+            quality: "unknown",
+            type: "embed"
+        }
+    }
 }
