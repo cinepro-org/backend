3c039b0a0f55587fa3adf6b206f29f36059f8d55
Inside4ndroid <inside4ndroid.techsup@gmail.com>
2025-02-17 08:26:49 +0000

    - added a regex check class before resolving embed links this makes the code more streamlined and easier to manage.

--- src/Controllers/Providers/2embed/2embed.js
+++ src/Controllers/Providers/2embed/2embed.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-import * as cheerio from "cheerio";
 import fetch from "node-fetch";
-import { resolve_streamwish } from "../../../utils/resolvers/streamwish.js";
+import { resolve } from "../../../utils/checkresolve.js";
 
 const URL = "https://www.2embed.cc";
 const PLAYER_URL = "https://uqloads.xyz";
@@ -27,7 +26,7 @@ export async function getTwoEmbed(params) {
             return new Error("No stream wish id found");
         }
 
-        streamUrl = await resolve_streamwish(`${PLAYER_URL}/e/${match.groups.id}`);
+        streamUrl = await resolve(`${PLAYER_URL}/e/${match.groups.id}`);
 
         if (!streamUrl) {
             return new Error("No stream found");
--- src/utils/resolvers/streamwish.js
+++ src/utils/resolvers/streamwish.js
@@ -1,46 +1,43 @@
 import fetch from 'node-fetch';
 import JsUnpacker from '../jsunpack.js';
 
-const regex = /(?:\/\/|\.)((?:(?:stream|flas|obey|sfast|str|embed|[mads]|cdn|asn|player|hls)?wish(?:embed|fast|only|srv)?|ajmidyad|atabkhha|atabknha|atabknhk|atabknhs|abkrzkr|abkrzkz|vidmoviesb|kharabnahs|hayaatieadhab|cilootv|tuktukcinema|doodporn|ankrzkz|volvovideo|strmwis|ankrznm|yadmalik|khadhnayad|eghjrutf|eghzrutw|playembed|egsyxurh|egtpgrvh|uqloads|javsw|cinemathek|trgsfjll|fsdcmo|anime4low|mohahhda|ma2d|dancima|swhoi|gsfqzmqu|jodwish|swdyu|katomen|iplayerhls|hlsflast|4yftwvrdz7|ghbrisk)\.(?:com|to|sbs|pro|xyz|store|top|site|online|me|shop|fun))(?:\/e\/|\/f\/|\/d\/)?([0-9a-zA-Z$:\/.]+)/;
 const referer = "https://www.2embed.cc/";
 
 export async function resolve_streamwish(url) {
-    if (regex.test(url)) {
-        try {
-            const response = await fetch(url, {
-                headers: {
-                    "Referer": referer
-                }
-            });
 
-            if (!response.ok) {
-                return null;
+    try {
+        const response = await fetch(url, {
+            headers: {
+                "Referer": referer
             }
+        });
 
-            const data = await response.text();
-
-            const packedDataRegex = /eval\(function(.*?)split.*\)\)\)/;
-
-            const packedDataMatch = data.match(packedDataRegex);
-            if (packedDataMatch) {
-                const packedJS = packedDataMatch[0];
-
-                const unpacker = new JsUnpacker(packedJS);
-                if (unpacker.detect()) {
-                    const unpackedJS = unpacker.unpack();
-                    const fileregex = /sources\:\[{file:"(.*?)"}/;
-                    const matcheuri = unpackedJS.match(fileregex);
-
-                    return matcheuri[1];
-
-                }
-            } else {
-                return null;
-            }
-        } catch (error) {
+        if (!response.ok) {
             return null;
         }
-    } else {
+
+        const data = await response.text();
+
+        const packedDataRegex = /eval\(function(.*?)split.*\)\)\)/;
+
+        const packedDataMatch = data.match(packedDataRegex);
+        if (packedDataMatch) {
+            const packedJS = packedDataMatch[0];
+
+            const unpacker = new JsUnpacker(packedJS);
+            if (unpacker.detect()) {
+                const unpackedJS = unpacker.unpack();
+                const fileregex = /sources\:\[{file:"(.*?)"}/;
+                const matcheuri = unpackedJS.match(fileregex);
+
+                return matcheuri[1];
+
+            }
+        } else {
+            return null;
+        }
+    } catch (error) {
         return null;
     }
+
 }
