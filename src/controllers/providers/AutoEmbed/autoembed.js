5c32922eca6a62e66f7e8570f6c06ff86892ce24
An0n-00 <142223573+An0n-00@users.noreply.github.com>
2025-03-02 17:48:26 +0100

    doing something (vidsrc.cc doesnt work anymore😭)
    (will fix it sometime)

--- src/controllers/providers/AutoEmbed/autoembed.js
+++ src/controllers/providers/AutoEmbed/autoembed.js
@@ -26,32 +26,13 @@ export async function getAutoembed(media) {
         if (data.error) {
             return new Error("No stream wish id found");
         }
-
-        const m3u8Url = data.videoSource;
-
-        const m3u8Response = await fetch(m3u8Url);
-        const m3u8Content = await m3u8Response.text();
-        const sources = parseM3U8(m3u8Content);
-
-        let vietnameseM3u8;
-        let vietnameseM3u8Response;
-        let vietnameseM3u8Content;
-
-        try {
-            vietnameseM3u8 = await getVietnameseUrl(media);
-        } catch (error) {
-            // ignore...
-        }
-
+        
         return {
             files: [
                 {
-                    file: m3u8Url,
+                    file: data.videoSource,
                     type: "hls",
-                    lang: "en",
-                    headers: {
-                        'Referer': 'https://autoembed.cc/'
-                    }
+                    lang: "en"
                 }
             ],
             subtitles: [
@@ -77,23 +58,4 @@ function mapSubtitles(subtitles) {
             type: type
         };
     });
-}
-
-async function getVietnameseUrl(media) {
-    let url;
-    if (media.type === "tv") {
-        url = `https://tom.autoembed.cc/api/getVideoSource?type=tv&id=${media.tmdbId}/${media.season}/${media.episode}`;
-    } else {
-        url = `https://viet.autoembed.cc/movie/${media.imdbId}`;
-    }
-    let data = await fetch(url, {
-        method: "GET",
-        headers: {
-            referer: "https://tom.autoembed.cc",
-            origin: "https://watch.autoembed.cc/",
-        }
-    });
-    let websiteHtml = await data.text();
-    let match = websiteHtml.match(/file: "([^"]+)"/);
-    return match ? match[1] : null;
 }
